58f2884b1a95efa29561c959e74f68f02b019f12
Amir142 <amiiir14.2@gmail.com>
2019-01-24 17:39:22 +0200

    asa

--- model.py
+++ model.py
@@ -5,6 +5,25 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
-# Write your classes here :
-class Product(Base):
-    pass
+# Place your database schema code here
+
+# Example code:
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key = True)
+    username = Column(String)
+    password = Column(String)
+    email = Column(String)
+    name = Column(String)
+    lastName = Column(String)
+    location = Column(String)
+
+    def __repr__(self):
+        return ("username: {}, password:{}, email:{}, name:{}, lastName:{}, location:{}".format(self.username, self.password, self.email, self.name, self.lastName, self.location))
+
+
+class Post(Base):
+    __tablename__ = "posts"
+    id = Column(Integer, primary_key = True)
+    category = Column(String)
+    description = Column(String)
